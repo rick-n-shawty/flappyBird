d1d55288a56cb2cb88e84894097567a21379cbbb
rick-n-shawty <sultanhoodie@gmail.com>
2024-06-05 19:22:18 +0800

    bird  class

---
+++ src/Bird.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#ifndef BIRD_HPP
+#define BIRD_HPP 
+
+class Bird{
+    public:
+        float x,y; 
+        int w,h;
+        Bird();
+        ~Bird();  
+
+        void draw(sf::RenderWindow& window){
+            window.draw(this->body);
+        }
+    private:
+        sf::RectangleShape body; 
+}
+
+
+#endif 
